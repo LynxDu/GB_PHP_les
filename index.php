1f3e8158c11daed44e86c1fcc0f94e0263013df8
Lynx <Hicode95>
2022-03-29 11:27:39 +0300

    +

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
-<?php
-//подгрузка данных
-require_once "funForIndex.php";
-$siteInfo = run();
-?>
-<head>
-    <title><?= $siteInfo['title'] ?></title>
-</head>
-<body>
-
-<h1><?= $siteInfo['h1'] ?> </h1>
-
-<p> <?= $siteInfo['time'] ?> </p>
-
-<h3> Раздел почемучка </h3>
-<div class = "content">
-    <?php
-
-    pochemuchka();
-
-    ?>
-</div>
-
-</body>
+<?php require_once "site/header.php"; ?>
+<div><?php require_once "site/lessons/les1/index.php"; ?></div>
+<?php require_once "site/footer.php"; ?>
---
+++ site/lessons/les1/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once "les1.php";
+use gb_php_ls\site\lessons\les1\les1;
+?>
+<h3> Раздел почемучка </h3>
+<?php
+$lesson = new les1();
+$lesson->pochemuchka();
+?>
