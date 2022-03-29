b16e99cf41c4d32206f33f03b1ad0c5ddfaa3d99
Lynx <Hicode95>
2022-03-30 01:47:34 +0300

    Урок 2. Условные блоки, ветвление, функции

--- lessons/les2/index.php
+++ lessons/les2/index.php
@@ -1,11 +1,7 @@
 <?php
-use site\lessons\les1\les2;
+require_once "les2.php";
+use site\lessons\les2\les2;
 ?>
-<h1><?= $siteInfo['h1'] ?> </h1>
-
-<p> <?= $siteInfo['time'] ?> </p>
-
-<h3> Раздел почемучка </h3>
 
 <?php
 
