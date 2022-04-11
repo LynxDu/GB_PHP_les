0bde4047752082797eddce7b0ba79e33fefe0523
Lynx <Hicode95>
2022-04-11 17:16:12 +0300

    Fix

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php
-require_once "funForIndex.php";
-$siteInfo = run();
-?>
-<head>
-    <title><?= $siteInfo['title'] ?></title>
-</head>
-<body>
+require_once "header.php";
 
-<h1><?= $siteInfo['h1'] ?> </h1>
 
-<p> <?= $siteInfo['time'] ?> </p>
 
-<h3> Раздел почемучка </h3>
-<div class = "content">
-    <?php
 
-    pochemuchka();
-
-    ?>
-</div>
-
-</body>
